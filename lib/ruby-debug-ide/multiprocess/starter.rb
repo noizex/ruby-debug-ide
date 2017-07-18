a6d087f8aa995994309c9f967e59637afd4d87a9
Kamil Niechajewicz <kamil.niechajewicz@sage.com>
2017-07-18 23:59:42 +0200

    Fixed split on null

--- lib/ruby-debug-ide/multiprocess/starter.rb
+++ lib/ruby-debug-ide/multiprocess/starter.rb
@@ -3,7 +3,7 @@ if ENV['IDE_PROCESS_DISPATCHER']
   ENV['DEBUGGER_STORED_RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
     next unless path =~ /ruby-debug-ide|ruby-debug-base|linecache|debase/
     $LOAD_PATH << path
-  end
+  end if ENV['DEBUGGER_STORED_RUBYLIB']
   require 'ruby-debug-ide'
   require 'ruby-debug-ide/multiprocess'
   Debugger::MultiProcess::do_monkey
